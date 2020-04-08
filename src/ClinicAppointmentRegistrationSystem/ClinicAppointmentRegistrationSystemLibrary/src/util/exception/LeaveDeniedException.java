86dd68dc1eb8304e492f9d9e711e1bc1983d00d0
Shei Er <50574151+NgSheiEr@users.noreply.github.com>
2020-04-08 15:50:42 +0800

    Update LeaveDeniedException.java

--- src/ClinicAppointmentRegistrationSystem/ClinicAppointmentRegistrationSystemLibrary/src/util/exception/LeaveDeniedException.java
+++ src/ClinicAppointmentRegistrationSystem/ClinicAppointmentRegistrationSystemLibrary/src/util/exception/LeaveDeniedException.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Group 11 IS2103 Pair Project
+ * Group members:
+ * - Gerwin Lee , A0184250L 
+ * - Ng Shei Er , A0185574R
+ * 
  */
 package util.exception;
 
